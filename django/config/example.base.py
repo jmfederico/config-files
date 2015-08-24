1e9bc7a87f1c314bd633f6dc266fb27702e8c0e4
Federco Jaramillo Martínez <federicojaramillom@gmail.com>
2015-08-23 20:06:15 -0500

    One extra dir for BASE_DIR

--- django/config/example.base.py
+++ django/config/example.base.py
@@ -59,7 +59,7 @@ DEBUG = bool(env_vars['DEBUG'])
 #
 ########################################################################
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env_vars['SECRET_KEY']
